59c9b40352c24e96cd527555c9fb36412bf0331e
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-05-15 11:11:54 +0100

    [ADD] Enabling of CORS policy in Program.cs for Angular comm

--- GameForum-DotNet8/GameForum-DotNet8/Program.cs
+++ GameForum-DotNet8/GameForum-DotNet8/Program.cs
@@ -5,6 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Enables communication with Angular (Cross-Origin Resource Sharing)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,12 +28,17 @@ builder.Services.AddDbContext<DataContext>(options =>
 });
 
 var app = builder.Build();
+app.UseCors("AllowAngularApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 // Middleware below (Would put authorization/authentication/services/repository methods below)
