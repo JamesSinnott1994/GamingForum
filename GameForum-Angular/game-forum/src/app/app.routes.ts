4b1c121a2e1ef66d14d394ce930ba2b2d28c6de5
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-05-15 13:41:14 +0100

    [ADD] Route to post works

--- GameForum-Angular/game-forum/src/app/app.routes.ts
+++ GameForum-Angular/game-forum/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'post/:id', component: PostPageComponent }
+];
--- GameForum-Angular/game-forum/src/app/post-list/post-list.component.ts
+++ GameForum-Angular/game-forum/src/app/post-list/post-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../models/post.model';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss'
 })
